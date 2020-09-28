9ca6ba9af1058808e1f0d96f8f8c005906edd13e
lee-man <minli.amoy@gmail.com>
2020-09-28 11:41:43 +0800

    update.

--- CIFAR_10/main.py
+++ CIFAR_10/main.py
@@ -12,6 +12,8 @@ import torch.nn as nn
 import torch.optim as optim
 
 import torchvision
+import torchvision.transforms as transforms
+
 
 from models import nin
 from torch.autograd import Variable
@@ -117,11 +119,23 @@ if __name__=='__main__':
     #     raise Exception\
     #             ('Please assign the correct data path with --data <DATA_PATH>')
 
-    trainset = torchvision.datasets.CIFAR10(root=args.data, train=True, download=True) #, transform=transform_train)
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(root=args.data, train=True, download=True, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
             shuffle=True, num_workers=2)
 
-    testset = torchvision.datasets.CIFAR10(root=args.data, train=False, download=True)
+    testset = torchvision.datasets.CIFAR10(root=args.data, train=False, download=True, transform=transform_test)
     testloader = torch.utils.data.DataLoader(testset, batch_size=100,
             shuffle=False, num_workers=2)
 
